8cf4b38bbd4bd3c3ed3aca595e4fc95eaf1538b0
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-08-04 13:26:58 +0200

    Created the file and folder for number game

---
+++ Number_Game/src/main/java/a/m/Number_Game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package a.m;
+
+/**
+ *
+ * @author USER
+ */
+public class Number_Game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
