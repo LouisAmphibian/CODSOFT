98bc9cd5cef0e8e3b3bf0d8e99af99527accb338
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-08-24 09:17:49 +0200

    Updated Frame

--- Number_Game/src/main/java/am/MyFrame.java
+++ Number_Game/src/main/java/am/MyFrame.java
@@ -4,8 +4,12 @@
  */
 package am;
 
-import java.awt.Frame;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -15,9 +19,26 @@ public class MyFrame extends JFrame {
     
     //global
     JFrame frame;
+    JPanel panelOne;
+    JLabel game_speech;
     
     //CREATING CONTRUCTOR FOR MY FRAME 
     MyFrame(){
+        
+        //label
+        game_speech = new JLabel();
+        game_speech.setText("""
+                            <html>I'm thinking of a number between 1 and 100.
+                            <br><br>You have 6 attempts to guess the number.</html>""");
+        game_speech.setBorder(new EmptyBorder(10,10,10,10)); //Adding marging to the lsbel
+        
+        //The first panel 
+        panelOne = new JPanel();
+        panelOne.setBounds(115,100,250,300);
+        panelOne.setLayout(new BorderLayout());        
+        panelOne.setBackground(new Color(192, 192, 192)); 
+        panelOne.add(game_speech,BorderLayout.NORTH);
+        
         
         frame = new JFrame();
         frame.setTitle("Number Game");
@@ -26,5 +47,6 @@ public class MyFrame extends JFrame {
         frame.setSize(500,500);
         frame.setLayout(null);
         frame.setVisible(true);
+        frame.add(panelOne);
     }
 }
