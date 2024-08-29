3b84efde6ed905ff7f068cb093db8c595575c217
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-08-29 10:12:23 +0200

    Can take user input. And Uploaded GUI class

---
+++ Student_Grade_Calculator/src/main/java/am/GUI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package am;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author USER
+ */
+class GUI extends JFrame{
+    
+    //declare 
+    private JTextField[] marksFields;
+    private JTextField total, avg, grade;
+    
+    public GUI(int numberOfSubject){
+        
+    }
+}
--- Student_Grade_Calculator/src/main/java/am/Student_Grade_Calculator.java
+++ Student_Grade_Calculator/src/main/java/am/Student_Grade_Calculator.java
@@ -4,6 +4,9 @@
 
 package am;
 
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author USER
@@ -11,6 +14,32 @@ package am;
 public class Student_Grade_Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //DECLARE
+        int numberOfSubjects;
+        
+        //the exception hanlde 
+        try{
+            //prompting the usser to enter subjects
+            String input = JOptionPane.showInputDialog(null, "Enter the number of subjects:");
+            numberOfSubjects = Integer.parseInt(input);
+            
+            //Condition for number size must be above 0
+            if(numberOfSubjects<= 0){
+                JOptionPane.showMessageDialog(null, "Please enter a valid number of subjects.");
+                return;
+            }else{
+              //For thread safety
+              SwingUtilities.invokeLater(() -> {  
+              GUI graadeCalculator = new GUI(numberOfSubjects);
+              graadeCalculator.setVisible(true);
+              });
+            }
+            
+        }catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Please enter a valid number of subjects.");
+            return;
+        }
+        
     }
 }
